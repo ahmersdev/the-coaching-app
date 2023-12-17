02f027c23490a31897639944e64062ed5aa38eec
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2023-12-17 14:02:08 +0500

    build error resolved.

--- app/_utils/getFontValue.tsx
+++ app/_utils/getFontValue.tsx
@@ -1,41 +1,3 @@
-import { useTheme } from "@mui/material/styles";
-import useResponsive from "../_hooks/useResponsive";
-
-// ----------------------------------------------------------------------
-
-export default function GetFontValue(variant: any) {
-  const theme: any = useTheme();
-  const breakpoints = useWidth();
-
-  const key = theme.breakpoints.up(breakpoints === "xl" ? "lg" : breakpoints);
-
-  const hasResponsive =
-    variant === "h1" ||
-    variant === "h2" ||
-    variant === "h3" ||
-    variant === "h4" ||
-    variant === "h5" ||
-    variant === "h6";
-
-  const getFont =
-    hasResponsive && theme.typography[variant][key]
-      ? theme.typography[variant][key]
-      : theme.typography[variant];
-
-  const fontSize = remToPx(getFont.fontSize);
-  const lineHeight = Number(theme.typography[variant].lineHeight) * fontSize;
-  const { fontWeight } = theme.typography[variant];
-  const { letterSpacing } = theme.typography[variant];
-
-  return { fontSize, lineHeight, fontWeight, letterSpacing };
-}
-
-// ----------------------------------------------------------------------
-
-export function remToPx(value: any) {
-  return Math.round(parseFloat(value) * 16);
-}
-
 export function pxToRem(value: any) {
   return `${value / 16}rem`;
 }
@@ -43,6 +5,9 @@ export function pxToRem(value: any) {
 export function responsiveFontSizes({ xs, md, lg }: any) {
   return {
     "@media (min-width:600px)": {
+      fontSize: pxToRem(xs),
+    },
+    "@media (max-width:600px)": {
       fontSize: pxToRem(xs),
     },
     "@media (min-width:900px)": {
@@ -53,16 +18,3 @@ export function responsiveFontSizes({ xs, md, lg }: any) {
     },
   };
 }
-
-// ----------------------------------------------------------------------
-
-const useWidth = () => {
-  const theme = useTheme();
-  const keys = [...theme.breakpoints.keys].reverse();
-  return (
-    keys.reduce((output: any, key: any) => {
-      const matches = useResponsive("up", key);
-      return !output && matches ? key : output;
-    }, null) || "xs"
-  );
-};
