8ec3c6299fe14b958e8dfe04c0351fed78179a91
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2023-12-31 13:47:14 +0500

    complaints

--- app/_sections/system-admin/complaints/index.tsx
+++ app/_sections/system-admin/complaints/index.tsx
@@ -1,5 +1,61 @@
+"use client";
+
+import { Avatar, Box, Button, Typography } from "@mui/material";
+import { Fragment, useState } from "react";
+import { complaintsDataArray } from "./complaints.data";
+import Reply from "./reply";
+
 const Complaints = () => {
-  return <>Complaints</>;
+  const [reply, setReply] = useState(false);
+
+  return (
+    <Fragment>
+      <Typography variant={"h5"} fontWeight={700} pb={1}>
+        All complaints from different clients
+      </Typography>
+
+      {complaintsDataArray?.map((item: any, index: any) => (
+        <Box
+          key={index}
+          bgcolor={"secondary.main"}
+          borderRadius={3}
+          padding={2.4}
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          my={1}
+          gap={1}
+        >
+          <Box display={"flex"} alignItems={"center"} gap={2}>
+            <Avatar src={item?.src} variant="rounded" />
+            <Typography variant={"h5"} fontWeight={400}>
+              {item?.message}
+            </Typography>
+          </Box>
+          <Button
+            variant={"contained"}
+            onClick={() => setReply(true)}
+            sx={{
+              color: "grey.100",
+              borderRadius: 25,
+              border: "1px dashed",
+              borderColor: "grey.100",
+              background: "transparent",
+              ":hover": {
+                backgroundColor: "grey.100",
+                color: "grey.900",
+              },
+            }}
+            disableElevation
+          >
+            Reply
+          </Button>
+        </Box>
+      ))}
+
+      {reply && <Reply reply={reply} setReply={setReply} />}
+    </Fragment>
+  );
 };
 
 export default Complaints;
