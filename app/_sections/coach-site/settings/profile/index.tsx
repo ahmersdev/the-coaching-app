8e7dcece7c0aea8d9546e9754ab83bb555f57f48
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2024-02-02 15:07:20 +0500

    fix

--- app/_sections/coach-site/settings/profile/index.tsx
+++ app/_sections/coach-site/settings/profile/index.tsx
@@ -1,3 +1,19 @@
+import { Stack } from "@mui/material";
+import MyProfile from "./my-profile";
+import About from "./about";
+import Password from "./password";
+import Address from "./address";
+
 export default function Profile() {
-  return <>Profile</>;
+  return (
+    <Stack direction={"column"} spacing={2}>
+      <MyProfile />
+
+      <About />
+
+      <Password />
+
+      <Address />
+    </Stack>
+  );
 }
