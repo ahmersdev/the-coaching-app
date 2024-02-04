4b6e524a56898c1dedea3a5f34344252ca4a2350
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2024-02-05 00:22:17 +0500

    diet

--- app/_sections/coach-site/clients/assign-diet/index.tsx
+++ app/_sections/coach-site/clients/assign-diet/index.tsx
@@ -21,6 +21,7 @@ import { LoadingButton } from "@mui/lab";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import * as Yup from "yup";
+import { useEffect } from "react";
 
 const dietValidationSchema: any = Yup?.object()?.shape({
   mealName: Yup?.string()?.trim()?.required("Required"),
@@ -42,8 +43,9 @@ export default function AssignDiet() {
     defaultValues: dietFormDefaultValues,
   });
 
-  const { handleSubmit, reset, control } = methods;
+  const { handleSubmit, reset, control, setValue, getValues } = methods;
 
+  // Meal 01 Diet Fields Handling
   const {
     fields: fieldsDiet,
     append: appendDiet,
@@ -53,45 +55,50 @@ export default function AssignDiet() {
     name: "diet",
   });
 
-  const onSubmit = async (data: any) => {
-    const meal1 = {
-      mealName: data?.mealName || "",
-      note: data?.note || "",
-      diet: [
-        {
-          includes: data?.includes || "",
-          quantity: data?.quantity || "",
-        },
-        ...data?.diet?.map((item: any) => ({
-          includes: item.includes || "",
-          quantity: item.quantity || "",
-        })),
-      ],
-    };
-    const otherMeals = data?.meals?.map((meal: any) => ({
-      mealName: meal?.mealName || "",
-      includes: meal?.includes || "",
-      quantity: meal?.quantity || "",
-      note: meal?.note || "",
-      diet: data?.diet?.map((item: any) => ({
-        includes: item?.includes || "",
-        quantity: item?.quantity || "",
-      })),
-    }));
-    console.log(meal1, otherMeals);
-    enqueueSnackbar("Diet Added Successfully!", {
-      variant: "success",
-    });
-    reset();
-    removeDiet();
-  };
-
   const handleAddDiet = () => {
     appendDiet({ includes: "", quantity: "" });
   };
 
   const handleRemoveDiet = (dietIndex: any) => {
     removeDiet(dietIndex);
+  };
+
+  // Remaining Meals Field Handling
+  const {
+    fields: fieldsMeals,
+    append: appendMeals,
+    remove: removeMeals,
+  } = useFieldArray({
+    control,
+    name: "meals",
+  });
+
+  const handleAddMeals = () => {
+    appendMeals({});
+  };
+
+  const handleRemoveMeals = (mealIndex: any) => {
+    removeMeals(mealIndex);
+  };
+
+  const handleAddMealsDiet = (mealIndex: any) => {
+    const mealValue = getValues(`meals[${mealIndex}].mealDiet`) || [];
+    setValue(`meals[${mealIndex}].mealDiet`, [...mealValue, {}]);
+  };
+
+  useEffect(() => {}, [getValues()]);
+
+  const handleRemoveMealsDiet = (mealIndex: any, mealDietIndex: any) => {
+    const mealValue = getValues(`meals[${mealIndex}].mealDiet`) || [];
+    mealValue.splice(mealDietIndex, 1);
+    setValue(`meals[${mealIndex}].mealDiet`, mealValue);
+  };
+
+  const onSubmit = async (data: any) => {
+    console.log(data);
+    enqueueSnackbar("Diet Added Successfully!", {
+      variant: "success",
+    });
   };
 
   return (
@@ -286,6 +293,176 @@ export default function AssignDiet() {
               </Accordion>
             </Box>
 
+            {fieldsMeals?.map((meal: any, mealIndex) => (
+              <Box
+                bgcolor={"secondary.900"}
+                key={meal?.id}
+                borderRadius={3}
+                mt={2}
+              >
+                <Accordion
+                  elevation={0}
+                  sx={{
+                    bgcolor: "transparent",
+                    p: 1,
+                    "&.Mui-expanded": {
+                      margin: 0,
+                    },
+                  }}
+                >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon sx={{ color: "grey.100" }} />}
+                  >
+                    <Box
+                      width={"100%"}
+                      display={"flex"}
+                      alignItems={"center"}
+                      justifyContent={"space-between"}
+                      gap={1}
+                    >
+                      <Box display={"flex"} alignItems={"center"} gap={1}>
+                        <MealIcon />
+                        <Typography
+                          variant={"h6"}
+                          color={"grey.100"}
+                          fontWeight={700}
+                        >
+                          Meal 0{mealIndex + 2}
+                        </Typography>
+                      </Box>
+                      <Typography
+                        variant={"body1"}
+                        color={"grey.100"}
+                        fontWeight={900}
+                        onClick={() => handleRemoveMeals?.(mealIndex)}
+                        mr={2}
+                        sx={{ cursor: "pointer" }}
+                      >
+                        X
+                      </Typography>
+                    </Box>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Grid container spacing={1}>
+                      <Grid item xs={12} md={6}>
+                        <RHFTextField
+                          name={`meals[${mealIndex}].mealName`}
+                          label={"Meal Name"}
+                          placeholder={"Enter Meal Name"}
+                        />
+                      </Grid>
+                      <Grid item xs={0} md={12} />
+                      <Grid item xs={12}>
+                        <Grid container spacing={1} alignItems={"end"}>
+                          <Grid item xs={12} md={3}>
+                            <RHFTextField
+                              name={`meals[${mealIndex}].includes`}
+                              label={"Includes"}
+                              placeholder={"-----"}
+                            />
+                            <RHFTextField
+                              name={`meals[${mealIndex}].quantity`}
+                              placeholder={"Enter Quantity"}
+                              type={"number"}
+                            />
+                          </Grid>
+
+                          {meal?.mealDiet &&
+                            meal?.mealDiet?.map(
+                              (diet: any, mealDietIndex: any) => (
+                                <Grid
+                                  item
+                                  xs={12}
+                                  md={3}
+                                  key={diet.id}
+                                  position={"relative"}
+                                >
+                                  <Typography
+                                    variant={"body1"}
+                                    color={"grey.100"}
+                                    fontWeight={900}
+                                    onClick={() =>
+                                      handleRemoveMealsDiet(
+                                        mealIndex,
+                                        mealDietIndex
+                                      )
+                                    }
+                                    sx={{ cursor: "pointer" }}
+                                    position={"absolute"}
+                                    right={5}
+                                  >
+                                    X
+                                  </Typography>
+                                  <RHFTextField
+                                    name={`meals[${mealIndex}].mealDiet[${mealDietIndex}].includes`}
+                                    label={"Includes"}
+                                    placeholder={"-----"}
+                                  />
+                                  <RHFTextField
+                                    name={`meals[${mealIndex}].mealDiet[${mealDietIndex}].quantity`}
+                                    placeholder={"Enter Quantity"}
+                                    type={"number"}
+                                  />
+                                </Grid>
+                              )
+                            )}
+
+                          <Button
+                            variant={"contained"}
+                            startIcon={<AddCircleIcon />}
+                            sx={{
+                              color: "grey.100",
+                              borderRadius: 25,
+                              height: 36,
+                              width: 90,
+                              border: "1px dashed",
+                              borderColor: "grey.100",
+                              background: "transparent",
+                              ml: 1,
+                              ":hover": {
+                                backgroundColor: "grey.100",
+                                color: "grey.900",
+                              },
+                            }}
+                            disableElevation
+                            onClick={() => handleAddMealsDiet(mealIndex)}
+                          >
+                            Add
+                          </Button>
+                        </Grid>
+                      </Grid>
+
+                      <Grid item xs={12} md={6}>
+                        <RHFTextField
+                          name={`meals[${mealIndex}].note`}
+                          label={"Add Note"}
+                          placeholder={"Add Some Details"}
+                          multiline
+                          rows={3}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Button
+                          variant={"contained"}
+                          sx={{
+                            color: "grey.100",
+                            width: 132,
+                            borderRadius: 25,
+                            border: "1px solid",
+                            borderColor: "primary.main",
+                          }}
+                          disableElevation
+                          type={"submit"}
+                        >
+                          Add
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
+            ))}
+
             <Button
               variant={"contained"}
               fullWidth
@@ -303,6 +480,7 @@ export default function AssignDiet() {
                 },
               }}
               disableElevation
+              onClick={handleAddMeals}
             >
               Add Another Meal
             </Button>
