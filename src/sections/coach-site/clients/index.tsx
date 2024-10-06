e0eda94df30dff5bd34a7e15d29caa2cdf50350a
Muhammad Ahmer Akram <mahmerakram6@gmail.com>
2024-10-06 16:54:25 +0500

    clients main page

--- src/sections/coach-site/clients/index.tsx
+++ src/sections/coach-site/clients/index.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { Avatar, Box, Button, Typography } from "@mui/material";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow } from "swiper/modules";
+import { Avatar, Box, Button, Grid, Typography } from "@mui/material";
 import { clientsColumns } from "./clients.data";
 import Link from "next/link";
 import { COACH_SITE } from "@/constants/routes";
@@ -17,86 +15,69 @@ export default function Clients() {
       <Typography variant={"h2"} color={"grey.100"} mb={2}>
         Latest Client Progressed
       </Typography>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={1}
-        spaceBetween={30}
-        speed={500}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 0,
-          modifier: 0,
-        }}
-        modules={[EffectCoverflow]}
-        className="mySwiper"
-        style={{ width: "100%" }}
-        breakpoints={{
-          900: {
-            slidesPerView: data?.clients.length < 6 ? data?.clients.length : 5,
-          },
-        }}
-      >
-        {sortedData?.map((item: any, index: number) => (
-          <SwiperSlide key={index}>
-            <Box
-              bgcolor={"secondary.main"}
-              borderRadius={3}
-              padding={"16px 18px"}
-              display={"flex"}
-              alignItems={"center"}
-              justifyContent={"space-between"}
-              flexDirection={"column"}
-              gap={2}
-              height={"100%"}
-            >
-              <Avatar
-                src={item?.profile_picture}
-                sx={{ width: 44, height: 44 }}
-              />
-              <Box textAlign={"center"}>
-                <Typography variant={"h6"} fontWeight={600}>
-                  {item?.full_name}
-                </Typography>
-                <Typography
-                  variant={"body1"}
-                  fontWeight={600}
-                  color={"grey.400"}
+
+      {isLoading ||
+        isFetching ||
+        (!isError && (
+          <Grid container spacing={2}>
+            {sortedData?.map((item: any, index: number) => (
+              <Grid item xs={12} sm={6} md={3} key={index}>
+                <Box
+                  bgcolor={"secondary.main"}
+                  borderRadius={3}
+                  padding={"16px 18px"}
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                  flexDirection={"column"}
+                  gap={2}
+                  height={"100%"}
                 >
-                  @{item?.username}
-                </Typography>
-              </Box>
-              <Link
-                href={{
-                  pathname: COACH_SITE?.CLIENTS_OVERVIEW,
-                  query: { clientId: item?.client_id },
-                }}
-              >
-                <Button
-                  variant={"contained"}
-                  sx={{
-                    color: "grey.100",
-                    borderRadius: 25,
-                    border: "1px dashed",
-                    borderColor: "grey.100",
-                    background: "transparent",
-                    ":hover": {
-                      backgroundColor: "grey.100",
-                      color: "grey.900",
-                    },
-                  }}
-                  disableElevation
-                >
-                  View Progress
-                </Button>
-              </Link>
-            </Box>
-          </SwiperSlide>
+                  <Avatar
+                    src={item?.profile_picture}
+                    sx={{ width: 44, height: 44 }}
+                  />
+                  <Box textAlign={"center"}>
+                    <Typography variant={"h6"} fontWeight={600}>
+                      {item?.full_name}
+                    </Typography>
+                    <Typography
+                      variant={"body1"}
+                      fontWeight={600}
+                      color={"grey.400"}
+                    >
+                      @{item?.username}
+                    </Typography>
+                  </Box>
+                  <Link
+                    href={{
+                      pathname: COACH_SITE?.CLIENTS_OVERVIEW,
+                      query: { clientId: item?.client_id },
+                    }}
+                  >
+                    <Button
+                      variant={"contained"}
+                      sx={{
+                        color: "grey.100",
+                        borderRadius: 25,
+                        border: "1px dashed",
+                        borderColor: "grey.100",
+                        background: "transparent",
+                        ":hover": {
+                          backgroundColor: "grey.100",
+                          color: "grey.900",
+                        },
+                      }}
+                      disableElevation
+                    >
+                      View Progress
+                    </Button>
+                  </Link>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
         ))}
-      </Swiper>
 
       <Box mt={2}>
         <TanstackTable
--- src/sections/coach-site/clients/use-clients.tsx
+++ src/sections/coach-site/clients/use-clients.tsx
@@ -1,13 +1,15 @@
 import { useGetClientDetailsQuery } from "@/services/coach-site/clients";
-import { IDataSorting } from "./cleints.interface";
+import { IDataSorting } from "./clients.interface";
 
 export default function useClients() {
   const { data, isLoading, isFetching, isError } = useGetClientDetailsQuery({});
 
-  const sortedData = [...(data?.clients || [])]?.sort(
-    (a: IDataSorting, b: IDataSorting) =>
-      new Date(b?.updatedAt)?.getTime() - new Date(a?.updatedAt)?.getTime()
-  );
+  const sortedData = [...(data?.clients || [])]
+    ?.sort(
+      (a: IDataSorting, b: IDataSorting) =>
+        new Date(b?.updatedAt)?.getTime() - new Date(a?.updatedAt)?.getTime()
+    )
+    ?.slice(0, 4);
 
   return { data, isLoading, isFetching, isError, sortedData };
 }
